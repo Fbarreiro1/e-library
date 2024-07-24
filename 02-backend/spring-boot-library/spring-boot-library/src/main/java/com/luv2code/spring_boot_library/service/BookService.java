23e57a030085d3ae82395fc260435c9d1cf7e68e
Federico Barreiro <fed_eh@hotmail.com>
2024-07-23 22:36:45 -0300

    Update BookService.java

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/service/BookService.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/service/BookService.java
@@ -53,6 +53,10 @@ public class BookService {
         return book.get();
     }
 
+       public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
     public boolean checkoutBookByUser(String userEmail, Long bookId) {
         Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
         return validateCheckout != null;
