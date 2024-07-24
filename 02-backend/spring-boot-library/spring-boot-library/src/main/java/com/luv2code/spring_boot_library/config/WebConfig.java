c1375d1a20b37645e7f1729d8506df797b355c1b
Federico Barreiro <fed_eh@hotmail.com>
2024-07-23 22:26:06 -0300

    Update WebConfig.java

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/config/WebConfig.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("https://e-library-production-beae.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
